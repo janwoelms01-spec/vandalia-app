545e1b80b045b18d55b08a32fa76eca7a9fd7254
xzudemimino <janwoelms01@gmail.com>
2026-03-02 19:30:37 +0100

    Update Links.tsx

--- src/component/Links.tsx
+++ src/component/Links.tsx
@@ -24,7 +24,7 @@ export function Links({role}: {role: Role}) {
     );
 
     return (
-        <nav className="bg-inherit w-full flex flex-col">
+        <nav >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     {/* spacer left if needed or logo moved to layout */}
