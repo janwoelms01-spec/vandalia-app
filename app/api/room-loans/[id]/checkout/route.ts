f010159d41c9095c26a285a978078bf93c081a6f
xzudemimino <janwoelms01@gmail.com>
2026-03-04 11:04:35 +0100

    nextrequest

--- app/api/room-loans/[id]/checkout/route.ts
+++ app/api/room-loans/[id]/checkout/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { PrismaClient, copies_state, room_loans_status } from "@prisma/client";
 import { requireApiPermission } from "@/lib/api/requireApiPermissions";
 
